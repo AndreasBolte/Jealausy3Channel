fdb7af148065937c83b8308c2a3507c458c9f02c
Andreas Bolte <andreas.bolte@colmuspro.de>
2024-05-09 14:16:00 +0200

    nodRed variables working

--- Jealausy2Channel/Jealousy2Channel/Jealousy2Channel.ino
+++ Jealausy2Channel/Jealousy2Channel/Jealousy2Channel.ino
@@ -20,7 +20,11 @@ const int DI_CHANNEL1UP_RC = CONTROLLINO_A0;    //AO: Digital Input CHANNEL 1 UP
 const int DI_CHANNEL1DOWN_RC = CONTROLLINO_A1;  //A1: Digital Input CHANNEL 1 DOWN from Remote Controll
 const int DI_CHANNEL2UP_RC = CONTROLLINO_A2;    //A2: Digital Input CHANNEL 2 UP from Remote Controll
 const int DI_CHANNEL2DOWN_RC = CONTROLLINO_A3;  //A3: Digital Input CHANNEL 2 DOWN from Remote Controll
+
+// Integer constants for interupts inputs
+const int DI_INTERRUPT_WIND = CONTROLLINO_IN0;  //IN0: Interrupt Input WIND
 const int DI_INTERRUPT_RAIN = CONTROLLINO_IN1;  //IN1: Interrupt Input RAIN
+
 // A4+A5 only AnalogInputs
 const int AI_SOLAR_HEATING_EXTERN = CONTROLLINO_A4;  //A5: Analog Input Heating Controll
 const int AI_ROOM_TEMPERATURE = CONTROLLINO_A5;      //A5: Analog Input temperature
@@ -49,19 +53,35 @@ int DI_RC_Channel2DownOld = 0;
 int DI_RC_Channel2DownFlank1 = 0;
 int DI_RC_Channel2DownFlank0 = 0;
 
-int DI_Wind = 0;             //Wind
-int DI_Rain = 0;             //Rain
+int DI_Wind = 0;  //Wind
+int DI_Rain = 0;  //Rain
 int DI_RainOld = 0;
 int DI_RainFlank1 = 0;
 int DI_RainFlank0 = 0;
+int windCounter = 0;
 
-long int i = 0;      // Loop counter ca 120 s
+long int i = 0;        // Loop counter ca 120 s
 long int i0 = 100000;  // fix i0
 long int i1 = 200000;  // fix i1
 long int i2 = 300000;  // fix i2
 long int i3 = 400000;  // fix i3
 long int i4 = 500000;  // fix i4
-long int iMax = 1000000;  
+long int i5 = 600000;  // fix i5
+long int i6 = 700000;  // fix i6
+long int iMax = 700000;
+
+// values for windSpeed
+unsigned long timeNew = 0;
+unsigned long timeOld = 0;
+float windSpeed = 0;
+const float SPEED_PRO_IMPULS = 1.2;  // from the description Vron other sensor 1,2 schwizer 2.4
+long int j = 0;                           // Loop counter ca 120 s
+long int jMax = 100000;
+
+void windCount() {
+  // increment the counter
+  ++windCounter;
+}
 
 
 
@@ -82,7 +102,12 @@ void setup() {
   pinMode(CONTROLLINO_A2, INPUT);
   pinMode(CONTROLLINO_A3, INPUT);
   // A4+A5 only AnalogInputs
-  pinMode(CONTROLLINO_IN1, INPUT);
+  // initialize necessary pin as interrupt pin
+  pinMode(DI_INTERRUPT_WIND, INPUT);
+  attachInterrupt(digitalPinToInterrupt(DI_INTERRUPT_WIND), windCount, RISING);
+  pinMode(DI_INTERRUPT_RAIN, INPUT);
+
+
 
 
 
@@ -92,7 +117,7 @@ void setup() {
 }  //End void setup()
 
 void loop() {
-  
+
   // send data only when available:
   if (Serial.available() > 0) {
     //serialAvailable();  // UserInterface.h
@@ -147,7 +172,7 @@ void loop() {
   DI_RC_Channel1UpOld = DI_RC_Channel1Up;
   if (DI_RC_Channel1UpFlank0 == 1) Serial.println("x001:0");
 
-   // DI_RC_Channel1Down flanks High and Low x002:1
+  // DI_RC_Channel1Down flanks High and Low x002:1
   if ((DI_RC_Channel1Down == 1) && (DI_RC_Channel1DownOld == 0)) DI_RC_Channel1DownFlank1 = 1;
   else DI_RC_Channel1DownFlank1 = 0;
   if (DI_RC_Channel1DownFlank1 == 1) Serial.println("x002:1");
@@ -165,7 +190,7 @@ void loop() {
   DI_RC_Channel2UpOld = DI_RC_Channel2Up;
   if (DI_RC_Channel2UpFlank0 == 1) Serial.println("x003:0");
 
-   // DI_RC_Channel2Down flanks High and Low x004:1
+  // DI_RC_Channel2Down flanks High and Low x004:1
   if ((DI_RC_Channel2Down == 1) && (DI_RC_Channel2DownOld == 0)) DI_RC_Channel2DownFlank1 = 1;
   else DI_RC_Channel2DownFlank1 = 0;
   if (DI_RC_Channel2DownFlank1 == 1) Serial.println("x004:1");
@@ -184,20 +209,29 @@ void loop() {
   if (DI_RainFlank0 == 1) Serial.println("x005:0");
 
 
-    
+
   //send by polling time
-    i = i + 1;
+  i = i + 1;
   if (DI_RC_Channel1Up == 0 && i == i0) Serial.println("x001:0");
   if (DI_RC_Channel1Up == 1 && i == i0) Serial.println("x001:1");
-  if (DI_RC_Channel1Down == 0 && i == i1) Serial.println("x002:0" );
+  if (DI_RC_Channel1Down == 0 && i == i1) Serial.println("x002:0");
   if (DI_RC_Channel1Down == 1 && i == i1) Serial.println("x002:1");
   if (DI_RC_Channel2Up == 0 && i == i2) Serial.println("x003:0");
   if (DI_RC_Channel2Up == 1 && i == i2) Serial.println("x003:1");
   if (DI_RC_Channel2Down == 0 && i == i3) Serial.println("x004:0");
   if (DI_RC_Channel2Down == 1 && i == i3) Serial.println("x004:1");
   if (DI_Rain == 0 && i == i4) Serial.println("x005:0");
-  if (DI_Rain == 1 && i == i4) Serial.println("x005:1");
-  if (i >= iMax) i = 0;
+  if (DI_Rain == 1 && i == i5) Serial.println("x005:1");
+  if (i == i6) {
+    timeNew = millis();
+    windSpeed = (float)((windCounter * SPEED_PRO_IMPULS * 3.6) / ((timeNew - timeOld)) * 1000);  // convert milis to seconds, unit km/h
+    windCounter = 0;
+    timeOld = timeNew;
+    Serial.print("x006:");
+    Serial.println(windSpeed);  // Node-Red Read
+ }  //end if (j == jMax)
+ if (i >= iMax) i = 0;
+
 
 
   // // write all Engines
